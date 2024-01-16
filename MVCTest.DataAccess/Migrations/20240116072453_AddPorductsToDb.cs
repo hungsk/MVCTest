19da7eaf29dd6b5336a62b1749b3a0e6c2825794
chen po hung <ski94114@skinsurance.com.tw>
2024-01-16 16:21:54 +0800

    加入Product

---
+++ MVCTest.DataAccess/Migrations/20240116072453_AddPorductsToDb.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCTest.DataAccess.Migrations
+{
+    public partial class AddPorductsToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Size = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Price = table.Column<double>(type: "float", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price", "Size" },
+                values: new object[] { 1, "天然果飲，迷人多變。", "台灣水果茶", 60.0, "大杯" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price", "Size" },
+                values: new object[] { 2, " 品鐵觀音，享人生的味道。", "鐵觀音", 35.0, "中杯" });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "Name", "Price", "Size" },
+                values: new object[] { 3, "用咖啡體悟悠閒時光。", "冰美式咖啡", 50.0, "中杯" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Products");
+        }
+    }
+}
---
+++ MVCTest.DataAccess/Migrations/20240116081836_AddCategoryToDbAndSeedTable2.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MVCTest.DataAccess.Migrations
+{
+    public partial class AddCategoryToDbAndSeedTable2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
