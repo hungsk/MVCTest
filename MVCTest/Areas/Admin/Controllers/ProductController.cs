720a98a95ba2e5d463336f77dc0123a458630429
chen po hung <ski94114@skinsurance.com.tw>
2024-01-16 17:01:01 +0800

    update

---
+++ MVCTest/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCTest.DataAccess.Data;
+using MVCTest.Models;
+
+namespace MVCTest.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _db.Products.ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Add(obj);
+                _db.SaveChanges();
+
+                TempData["success"] = "Product新增成功!";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? ProductFromDb = _db.Products.Find(id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Products.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product ProductFromDb = _db.Products.Find(id);
+            if (ProductFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(ProductFromDb);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult Delete(int? id)
+        {
+            Product? obj = _db.Products.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Products.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
